c4dc1fbf37794fdf4cc5792af9d61387009c798c
Cyrille Derche <c.derche@me.com>
2023-05-17 14:41:51 +0200

    add env var validation

---
+++ src/config/env.ts
@@ -0,0 +1,29 @@
+import Joi from 'joi'
+
+const schema = Joi
+  .object()
+  .keys({
+    PORT: Joi.number().required().default(3000),
+    MONGODB_HOST: Joi.string().required().example('localhost'),
+    MONGODB_PORT: Joi.number().required().example(27017),
+    MONGODB_USER: Joi.string().required().example('root'),
+    MONGODB_PASS: Joi.string().required().example('pass'),
+    MONGODB_NAME: Joi.string().optional().example('db'),
+    REDIS_QUEUE_HOST: Joi.string().optional().example('localhost'),
+    REDIS_QUEUE_PORT: Joi.number().optional().example(6379)
+  })
+  .unknown()
+
+const { value: env, error } = schema.prefs({ errors: { label: 'key' } }).validate(process.env)
+
+if (error != null) {
+  throw new Error(`Config validation error: ${error.message}`)
+}
+
+// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+const MONGODB_URL = `mongodb://${env.MONGODB_USER}:${env.MONGODB_PASS}@${env.MONGODB_HOST}:${env.MONGODB_PORT}/${env.MONGODB_NAME !== undefined ? env.MONGODB_NAME : ''}`
+console.log(`MONGODB_URL: ${MONGODB_URL}`)
+export default {
+  ...env,
+  MONGODB_URL
+}
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import app from './app'
 import mongoose from 'mongoose'
+import { env } from './config'
 
 mongoose.set('strictQuery', true)
-
-const PORT = Number(process.env.PORT)
-const MONGODB_URL = String(process.env.MONGODB_URL)
 
 main().catch(err => { console.error(err) })
 
 async function main (): Promise<void> {
-  await mongoose.connect(MONGODB_URL)
-  app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+  await mongoose.connect(env.MONGODB_URL)
+  app.listen(env.PORT, () => {
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    console.log(`⚡️[server]: Server is running at http://localhost:${env.PORT}`)
   })
 }
